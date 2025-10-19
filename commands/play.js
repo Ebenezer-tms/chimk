26807041505a7909b8723dfd507441d80cb9d5e5
Ebenezer-tms <cutesuper975@gmail.com>
2025-10-19 08:59:48 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,25 @@
 const yts = require('yt-search');
+const path = require('path');
 const axios = require('axios');
+const time = new Date().toLocaleTimeString();
 
 async function playCommand(sock, chatId, message) {
-    try {
+    try {        
+      //  const tempDir = path.join(__dirname, "temp");
+       // if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
+        
         const text = message.message?.conversation || message.message?.extendedTextMessage?.text;
         const searchQuery = text.split(' ').slice(1).join(' ').trim();
         
         if (!searchQuery) {
             return await sock.sendMessage(chatId, { 
-                text: "Which song do you want to download🤷‍♂️?"
+                text: "What song do you want to download?"
             });
         }
+        
+      //  const timestamp = Date.now();
+      //  const fileName = `audio_${timestamp}.mp3`;
+        //const filePath = path.join(tempDir, fileName);
 
         // Search for the song
         const { videos } = await yts(searchQuery);
@@ -22,7 +31,7 @@ async function playCommand(sock, chatId, message) {
 
         // Send loading message
         await sock.sendMessage(chatId, {
-            text: "_Please wait your download is in progress_"
+            text: "wait for your song it is in process"}, { quoted: message 
         });
 
         // Get the first video result
@@ -30,7 +39,7 @@ async function playCommand(sock, chatId, message) {
         const urlYt = video.url;
 
         // Fetch audio data from API
-        const response = await axios.get(`https://kaiz-apis.gleeze.com/api/yt-down?url=https%3A%2F%2Fyoutube.com%2Fwatch%3Fv%3DtsmPCi7NKrg&apikey=8e3b0d39-d9d4-47a1-a125-0801eb103e7f`);
+        const response = await axios.get(`https://api.privatezia.biz.id/api/downloader/ytmp3?url=${urlYt}`);
         const data = response.data;
 
         if (!data || !data.status || !data.result || !data.result.downloadUrl) {
@@ -45,6 +54,7 @@ async function playCommand(sock, chatId, message) {
         // Send the audio
         await sock.sendMessage(chatId, {
             audio: { url: audioUrl },
+         //   document: { url: filePath },
             mimetype: "audio/mpeg",
             fileName: `${title}.mp3`
         }, { quoted: message });
@@ -56,7 +66,7 @@ async function playCommand(sock, chatId, message) {
     } catch (error) {
         console.error('Error in song2 command:', error);
         await sock.sendMessage(chatId, { 
-            text: "Failed to retrieve the MP3 download link. Please try again later😅😅."
+            text: "Failedto retrieve your song in api. Please try again later😅😅."
         });
         
         //err react ❌
