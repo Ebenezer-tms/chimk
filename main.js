585c0f51109e285b9390237e380e031835a10900
Ebenezer-tms <cutesuper975@gmail.com>
2025-10-21 20:15:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -354,6 +354,8 @@ async function handleMessages(sock, messageUpdate, printLog) {
                 await helpCommand(sock, chatId, message, global.channelLink);
                 commandExecuted = true;
                 break;
+            case 'img': await imgCommand(sock, chatId, m); 
+                break
             case userMessage === '.sticker' || userMessage === '.s':
                 await stickerCommand(sock, chatId, message);
                 commandExecuted = true;
