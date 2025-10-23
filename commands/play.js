43be28c2ce21c97948107a74f3e97c66f3f3c891
Ebenezer-tms <cutesuper975@gmail.com>
2025-10-23 13:08:57 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -55,7 +55,7 @@ async function playCommand(sock, chatId, message) {
         await sock.sendMessage(chatId, {
             audio: { url: audioUrl },
          //   document: { url: filePath },
-            mimetype: "audio/mpeg",
+            mimetype: "document/mpeg",
             fileName: `${title}.mp3`
         }, { quoted: message });
         
