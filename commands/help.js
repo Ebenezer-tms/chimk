89344b1595f804da25542241d88571e3d219d27f
Ebenezer-tms <cutesuper975@gmail.com>
2025-10-30 23:54:56 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -60,7 +60,7 @@ const currentMode = data.isPublic ? 'public' : 'private';
 const hostName = detectHost();
     
     const helpMessage = `
-┏❐  *❴《 😍PRETTY-MD😍 》❵* ❐
+┏❐  *❴《 ${config.botName} 》❵* ❐
 ├◆ *Owner:* ${settings.botOwner}
 ├◆ *Mode:* ${currentMode}
 ├◆ *Host:* ${hostName}
