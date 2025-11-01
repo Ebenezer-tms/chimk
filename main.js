a87843303e67d76a91021a238615576878f6542e
Ebenezer-tms <cutesuper975@gmail.com>
2025-11-01 11:34:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -184,7 +184,6 @@ const aliveCommand = require('./commands/alive');
 const blurCommand = require('./commands/img-blur');
 const githubCommand = require('./commands/github');
 const uptimeCommand = require('./commands/uptime');
-const tutorialCommand = require('/commands/tutorial');
 /*━━━━━━━━━━━━━━━━━━━━*/
 
 /*━━━━━━━━━━━━━━━━━━━━*/
