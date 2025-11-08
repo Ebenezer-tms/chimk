b213b99719442f73700797ca9c9081a151bd1e02
Ebenezer-tms <cutesuper975@gmail.com>
2025-11-08 12:12:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.2",
+  version: "1.0.3",
   updateZipUrl: "https://github.com/Ebenezer-tms/chimk/archive/refs/heads/main.zip",
 };
 
