9c725f60084b287910e79b74f9fbece49786ac87
Ebenezer-tms <cutesuper975@gmail.com>
2025-11-23 21:21:26 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,26 +1,23 @@
 /*by supreme*/
-
-
 const os = require('os');
 const settings = require('../settings.js');
-const { getBotName, handleSetBotCommand } = require('./setbot')
+const { getBotName, handleSetBotCommand } = require('./setbot');
 let newBot = getBotName();
 
-async function pingCommand( sock, chatId, message ) {
+async function pingCommand( sock, chatId ) {
   try {
     const start = Date.now();
     const sentMsg = await sock.sendMessage(chatId, {
-      text: '*🔹pong!...*'},{ quoted: message
+      text: '*🔹pong!...*'
     });
 
     const ping = Date.now() - start;
-    const response = `*${newBot} Speed: ${ping} ms*`;
+    const response = `*🔸️speed🔹️:* ${ping} ms`;
 
     await sock.sendMessage(chatId, {
       text: response,
       edit: sentMsg.key// Edit the original message
-    });   
-    
+    });
   } catch (error) {
     console.error('Ping error:', error);
     await sock.sendMessage(chatId, { text: 'Failed to measure speed.' });
